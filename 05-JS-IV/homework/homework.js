4624e8ad63467333b3c330b31229b89bf96e05cf
Terry <danyel.martel@gmail.com>
2022-07-11 08:57:37 -0500

    CSS y HTMl realizados

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -65,6 +65,7 @@ function tieneEmail (usuario) {
   // Devuelve "true" si el usuario tiene un valor definido para la propiedad "email"
   // De lo contratio, devuelve "false"
   // Tu código:
+  console.log(usuario)
     if ( usuario["email"] ) {
     return true;
   } else return false;
